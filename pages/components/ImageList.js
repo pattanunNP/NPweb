4fb3d6218fc5cc1eee0c510af1e7507f60894eb1
PattanunNP <32350702+pattanunNP@users.noreply.github.com>
2020-09-25 00:50:30 +0700

    Update ImageList.js

--- pages/components/ImageList.js
+++ pages/components/ImageList.js
@@ -1,3 +1,4 @@
+import React from "react";
 const ImagesList = [
   "https://res.cloudinary.com/image-chatbot/image/upload/v1600952781/Portfolio/118199166_3478883002133320_3886264000866848638_n.jpg_p9rdpx.jpg",
   "https://res.cloudinary.com/image-chatbot/image/upload/v1600952658/Portfolio/118256440_1949323301870862_8566844396165459008_n_fjoxmv.jpg",
